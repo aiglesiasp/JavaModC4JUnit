a6ae139889bacbb187cd0767c249f7a8f2fc16c9
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-04-06 10:12:01 +0200

    Class Geometria Add App Class modified

--- JavaModC4JUnit/src/main/java/com/aiglesiasp/junit/JavaModC4JUnit/App.java
+++ JavaModC4JUnit/src/main/java/com/aiglesiasp/junit/JavaModC4JUnit/App.java
@@ -1,5 +1,7 @@
 package com.aiglesiasp.junit.JavaModC4JUnit;
 
+import com.aiglesiasp.junit.JavaModC4JUnit.dto.Geometria;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	//Creacion de Objeto Circulo
+ 	   Geometria gm=new Geometria(2);
+ 	   double areaCirc= gm.areaCirculo(2);
+ 	   gm.setArea(areaCirc);    
+ 	   System.out.println(gm);
     }
 }
