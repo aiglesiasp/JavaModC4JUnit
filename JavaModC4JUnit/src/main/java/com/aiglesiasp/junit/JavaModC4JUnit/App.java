48132168e57e7beee56be068673ae9a62f057d98
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-04-06 10:05:02 +0200

    First commit Maven project Created

---
+++ JavaModC4JUnit/src/main/java/com/aiglesiasp/junit/JavaModC4JUnit/App.java
@@ -0,0 +1,13 @@
+package com.aiglesiasp.junit.JavaModC4JUnit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
